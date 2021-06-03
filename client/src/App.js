f7dbbfbb4eff0d4c33ff6598c91c4a5ea1b4effa
nadira arasya <nadira.arasya.tif18@polban.ac.id>
2021-06-03 15:57:30 +0700

    navigation bar for all user types

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import { ThemeProvider } from '@material-ui/core';
 import Router from './Router';
+import { ThemeProvider } from '@material-ui/core';
+
+import Navbar from './components/Navbar/Navbar';
+import theme from './theme';
+
+const App = () => { 
 
 import Navbar from './components/Navbar/Navbar';
 import theme from './theme';
