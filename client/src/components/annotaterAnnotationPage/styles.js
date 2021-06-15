af7c6ab32bffcbd095343c3b22ffec2130180e51
nadira arasya <nadira.arasya.tif18@polban.ac.id>
2021-06-15 17:16:15 +0700

    annotation page finished ui

--- client/src/components/annotaterAnnotationPage/styles.js
+++ client/src/components/annotaterAnnotationPage/styles.js
@@ -2,11 +2,11 @@ import { makeStyles } from '@material-ui/core';
 
 export default makeStyles((theme) => ({
     pageTitle: {
-        marginLeft: '20px',
+        // marginLeft: '20px',
         marginTop: '30px',
     },
     taskLabel: {
-        marginLeft: '20px',
+        // marginLeft: '20px',
         marginTop: '10px',
     },
     labelContainer:{
@@ -14,13 +14,32 @@ export default makeStyles((theme) => ({
         gridTemplateColumns: '3fr 1fr'
     },
     toolsContainer: {
-        marginLeft: '20px',
+        // marginLeft: '20px',
     },
     logo: {
         maxWidth: '30',
     },
-    buttonContainer:{},
+    buttonContainer:{
+        padding: '20px 40px'
+    },
     rightContainer: {
-        height: '100%'
+        paddingLeft: '3%',
+        display: 'flex',
+        justifyContent: 'space-between',
+        flexDirection: 'column'
+
+    },
+    imageContainer: {
+        display:'flex', 
+        justifyContent: 'center', 
+        marginTop: '10px', 
+        marginRight:'20px', 
+        borderWidth: '1px', 
+        borderColor: '#CFCFCF', 
+        borderStyle: 'solid', 
+        borderRadius: '10px'
+    },
+    submitButtonContainer: {
+        
     }
 }))
