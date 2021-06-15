2630f934ad0db401a5e18062b8e55c58a028cc64
dwiana <dwiana.kamila.tif18@polban.ac.id>
2021-06-15 15:14:22 +0700

    add API downloadTask and get image by id task

--- server/routes/task.js
+++ server/routes/task.js
@@ -7,5 +7,6 @@ const router = express.Router();
 
 router.get('/',getTask);
 router.post('/createTask',findUser, createTask);
-
+router.get('/downloadTasks/:id',downloadTask);
+router.get('/getImageById/:id', getImageById);
 export default router;
