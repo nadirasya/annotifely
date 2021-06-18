b49df5b907a518187f272c72bb09f13dcfba6652
nadirasya <55722603+nadirasya@users.noreply.github.com>
2021-06-18 12:03:55 +0700

    Update tasks.js

--- client/src/actions/tasks.js
+++ client/src/actions/tasks.js
@@ -7,19 +7,9 @@ export const getTasks = () => async (dispatch) => {
     const currentDate = moment()
     try{
         const { data } = await api.fetchTasks();
-        let clients = await api.getClients();
-        clients = clients.data
         await data.map(async(task) => {
-            // console.log("clients", clients)
-            //Calculate time difference 
             const createdDate = moment(task.createdAt);
             task['timeRemaining'] =  currentDate.diff(createdDate, 'days');
-            //Get client name
-            const clientFound = clients.find(client=>client._id==task.idClient)
-            task['clientName'] = clientFound.name
-            // console.log('here')
-            // const client = await api.getClientById(task.idClient)
-            // task['clientName'] = client.data.name
         })
         console.log(data)
         dispatch({ type: FETCH_ALL, payload: data });
