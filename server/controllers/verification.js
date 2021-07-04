0161831fa570abc1836928cfd7a5830900953200
dwiana <dwiana.kamila.tif18@polban.ac.id>
2021-07-04 17:44:38 +0700

    Integrate Cloudinary

--- server/controllers/verification.js
+++ server/controllers/verification.js
@@ -6,7 +6,7 @@ export const createVerification = async (req, res) => {
     const verificationData = req.body;
     const total = verificationData.length
 
-    await verificationData.map(async(verification, index) => {
+    await verificationData?.map(async(verification, index) => {
     
 
     if(!verification.annotationId)
