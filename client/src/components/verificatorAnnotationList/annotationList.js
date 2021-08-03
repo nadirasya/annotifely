7fa5fbf6fe6dc120e600d48ab8592a14bf08a01b
nadira arasya <nadira.arasya.tif18@polban.ac.id>
2021-08-03 18:20:17 +0700

    verificator verification form update w form handling

--- client/src/components/verificatorAnnotationList/annotationList.js
+++ client/src/components/verificatorAnnotationList/annotationList.js
@@ -38,7 +38,7 @@ const AnnotationList = () => {
         dispatch(getAnnotations());
         clearTimeout(timer.current);
 
-    }, [dispatch, load===true])
+    }, [dispatch, load===true, annotations])
 
     timer.current = window.setTimeout(() => {
         setLoading(false);
@@ -61,7 +61,7 @@ const AnnotationList = () => {
                 <b> Annotations List</b> 
                 </Typography>
             </div>
-            { !annotations?.length ?
+            { annotations.length === 0 ?
                 <div>
                     { loading ? 
                         <div style={{display: 'flex', justifyContent: 'center'}}> 
@@ -97,7 +97,24 @@ const AnnotationList = () => {
                                 </TableRow>
                                 </TableHead>
                                 <TableBody>
-
+                                {annotations.map((annotation) => (
+                                    <TableRow key={annotation._id} style={{alignItems: "left"}}>
+                                    <TableCell component="th" scope="row">
+                                        <Typography variant="subtitle1" ><b>{annotation?.annotater[0]?.name}</b></Typography>
+                                    </TableCell>
+                                    <TableCell>{annotation?.task[0]?.title}</TableCell>
+                                    <TableCell>{annotation?.task[0]?.totalImage}</TableCell>
+                                    <TableCell>
+                                        {annotation.submitted === 0 ? 'Today' : annotation.submitted === 1 ? `${annotation.submitted} day ago` : `${annotation.submitted} days ago`}
+                                        {/* {annotation.submitted > '24 hours ago' ? 'Today' : `${annotation.submitted}`} */}
+                                    </TableCell>
+                                    <TableCell>
+                                        <Button variant="contained" disableElevation className={classes.buttonTertiary} onClick={() => handleReview(annotation.task[0]._id, annotation.annotater[0]._id)}>
+                                            <Typography variant="subtitle2">Review</Typography>
+                                        </Button>
+                                    </TableCell>
+                                    </TableRow>
+                                ))}
                                 </TableBody>
                             </Table>
                         </TableContainer>
---
+++ client/src/components/verificatorVerificationPage/SelectBox.js
@@ -0,0 +1,91 @@
+import React, { useEffect } from 'react';
+import { makeStyles, withStyles } from '@material-ui/core/styles';
+import InputLabel from '@material-ui/core/InputLabel';
+import MenuItem from '@material-ui/core/MenuItem';
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
+import NativeSelect from '@material-ui/core/NativeSelect';
+import InputBase from '@material-ui/core/InputBase';
+
+const BootstrapInput = withStyles((theme) => ({
+  root: {
+    'label + &': {
+    //   marginTop: theme.spacing(2),
+    },
+  },
+  input: {
+    borderRadius: 4,
+    position: 'relative',
+    backgroundColor: theme.palette.background.paper,
+    border: '1px solid #ced4da',
+    fontSize: 16,
+    padding: '10px 26px 10px 12px',
+    transition: theme.transitions.create(['border-color', 'box-shadow']),
+    // Use the system font instead of the default Roboto font.
+    fontFamily: [
+      '-apple-system',
+      'BlinkMacSystemFont',
+      '"Segoe UI"',
+      'Roboto',
+      '"Helvetica Neue"',
+      'Arial',
+      'sans-serif',
+      '"Apple Color Emoji"',
+      '"Segoe UI Emoji"',
+      '"Segoe UI Symbol"',
+    ].join(','),
+    '&:focus': {
+      borderRadius: 4,
+      borderColor: '#80bdff',
+      boxShadow: '0 0 0 0.2rem rgba(0,123,255,.25)',
+    },
+  },
+}))(InputBase);
+
+const useStyles = makeStyles((theme) => ({
+  margin: {
+    width:'100%',
+    marginBottom: theme.spacing(1)
+    // margin: theme.spacing(1),
+  },
+}));
+
+const SelectBox = ({label1, label2, label3, label4, disabled, selected, handleSelected, index}) => {
+  const classes = useStyles();
+  const [score, setScore] = React.useState(0);
+
+  const handleChange = (event) => {
+    handleSelected(event)
+    setScore(event.target.value);
+  };
+
+  useEffect(() => {
+    console.log("selected", selected)
+  }, [])
+
+  return (
+    <div>
+      <FormControl className={classes.margin} disabled={disabled}>
+        <Select
+          labelId="demo-customized-select-label"
+          id="demo-customized-select"
+          value={score}
+          name={index}
+          onChange={handleChange}
+          
+          input={<BootstrapInput />}
+        >
+          <MenuItem disabled value={0}>
+            <em>Please select one</em>
+          </MenuItem>
+          <MenuItem value={label1}>{label1}</MenuItem>
+          <MenuItem value={label2}>{label2}</MenuItem>
+          <MenuItem value={label3}>{label3}</MenuItem>
+          <MenuItem value={label4}>{label4}</MenuItem>
+        </Select>
+      </FormControl>
+    </div>
+  );
+}
+
+export default SelectBox;
